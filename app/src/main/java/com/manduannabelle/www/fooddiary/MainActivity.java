6b24158889182e8eb25071bf797d5963ba9eba18
Lingran Zha <lzha@cs.washington.edu>
2019-07-14 17:00:13 -0700

    complete some part of the main page layout

--- app/src/main/java/com/manduannabelle/www/fooddiary/MainActivity.java
+++ app/src/main/java/com/manduannabelle/www/fooddiary/MainActivity.java
@@ -2,6 +2,16 @@ package com.manduannabelle.www.fooddiary;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.Toolbar;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +19,48 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // for the date on the toolbar
+        Calendar calendar = Calendar.getInstance();
+        String currentDate = DateFormat.getDateInstance(DateFormat.DAY_OF_WEEK_IN_MONTH_FIELD).format(calendar.getTime());
+
+        TextView textViewDate = findViewById(R.id.text_view_date);
+        textViewDate.setText(currentDate);
+
+        // for tool bar to show
+        android.support.v7.widget.Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.theme:
+                Toast.makeText(this, "Theme selected", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.language:
+                Toast.makeText(this, "Language selected", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.feedback:
+                Toast.makeText(this, "Feedback selected", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.rateus:
+                Toast.makeText(this, "Rate us selected", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.about:
+                Toast.makeText(this, "About selected", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
     }
 }
